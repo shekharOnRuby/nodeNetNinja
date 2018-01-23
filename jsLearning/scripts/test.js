d3b8bc9e24fd526b6a15168c7d2cb6d908d8d3db
Shekhar Karande <shekhar.karande@gmail.com>
2018-01-23 18:52:24 +0000

    working with JS

--- jsLearning/scripts/test.js
+++ jsLearning/scripts/test.js
@@ -1,1 +1,15 @@
-alert("hello world")
+let youLikeMeat = true
+
+if(youLikeMeat){
+    document.write('you are a carnivore')
+    document.write("\n you might want to think about it")
+}
+
+let myNum = 10
+
+if (myNum == 11){
+    document.write('\nthe number is equal to 11')
+}
+else{
+    document.write('\nthe number is not equal to 11')
+}
