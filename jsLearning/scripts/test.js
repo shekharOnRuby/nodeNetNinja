c37cc0cbb787bcab456f0ad5cbf45cc63a7315c0
Shekhar Karande <shekhar.karande@gmail.com>
2018-01-24 10:46:51 +0000

    adding stuff

--- jsLearning/scripts/test.js
+++ jsLearning/scripts/test.js
@@ -1,15 +1,25 @@
 let youLikeMeat = true
 
 if(youLikeMeat){
-    document.write('you are a carnivore')
-    document.write("\n you might want to think about it")
+    document.write('<div>you are a carnivore</div>')
+    document.write('<div> you might want to think about it</div>')
 }
 
 let myNum = 10
 
 if (myNum == 11){
-    document.write('\nthe number is equal to 11')
+    document.write('\n the number is equal to 11')
 }
 else{
-    document.write('\nthe number is not equal to 11')
+    document.write('<div> the number is not equal to 11 </div>')
+}
+
+let links = document.getElementsByTagName("a")
+
+/*links.forEach(element => {
+    element.className = "link-" + 
+});*/
+
+for(i = 0; i <links.length ; i++){
+    links[i].className = "links-"+i
 }
